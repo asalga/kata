d82deba4e17253b2e54285947ffef02d928f5fd8
Andor <freelance.andor.salga@invivo.com>
2019-03-19 13:03:09 -0400

    converting to p5.

--- src/js/index.js
+++ src/js/index.js
@@ -1,127 +1,144 @@
 'use strict';
 
-import GameTimer from './core/GameTimer.js';
 import Utils from './Utils.js';
-import P3 from './P3.js';
-import Scene from './Scene.js';
-
 import Debug from './debug/Debug.js';
-import Event from './event/Event.js';
-import EventSystem from './event/EventSystem.js';
 
-// Systems
-import Renderer from './Renderer.js';
+window.setup = function(){
+  console.log('setup');
+  createCanvas(400, 400);
+}
 
-import cfg from './cfg.js';
-import Pool from './core/Pool.js';
+window.draw = function(){
+  background(100);
+  rect(0,0,100,100);
 
-
-// var sound = new Howl({
-//   src: ['../data/explosion.wav'],
-//   volume: 0.8
-// });
-
-// sound.play();
-
-window.gameTime = 0;
-window.gameFrameCount = 0;
-window.Renderer = Renderer;
-
-window.debug = false;
-window.Debug = Debug;
-window.scene = null;
-
-window.vec2Ctor = 0;
-window.clearArrayCalls = 0;
-
-window.Events = new EventSystem();
-window.ignoreDirty = false;
-window.p3 = null;
-
-let p3;
-let timer;
-let perfTimer = new Date();
-
-let avgDelta = 0;
-let avgFrames = 0;
-let avgCalc = 0;
-
-document.addEventListener('mousedown', e => new Event({ evtName: 'GAME_MOUSE_DOWN', data: e }).fire());
-document.addEventListener('mouseup', e => new Event({ evtName: 'GAME_MOUSE_UP', data: e }).fire());
-document.addEventListener('contextmenu', e => e.preventDefault());
-
-function update(dt) {
   Debug.add(`Game time: ${Math.floor(window.gameTime)}`);
-  Debug.add(`Root Entity count: ${scene.entities.size}`);
-
-  let totalVec2Calls = window.vec2Ctor.toLocaleString();
-  Debug.add(`Total Vec2 ctor calls: ${totalVec2Calls}`);
-  // Debug.add('Bullets: ' + window.count);
-
-  scene.update(dt);
-
-  Events.printDebug();
-  window.gameTime += dt;
-}
-
-function preRender() {
-  perfTimer = new Date().getTime();
-  Renderer.preRender();
-}
-
-function render() {
-  Renderer.render();
-  gameFrameCount++;
-}
-
-function postRender() {
-  let timeDiff = new Date().getTime() - perfTimer;
-  // avgDelta += timeDiff;
-  // avgFrames++;
-  // if (avgFrames > 100) {
-  //   avgCalc = avgDelta / avgFrames;
-  //   avgFrames = 0;
-  //   avgDelta = 0;
-  // }
-
-  Debug.add('render ms: ' + timeDiff);
-  // Debug.add('avg render ms: ' + avgCalc);
-  // Debug.add('clear array calls: ' + window.clearArrayCalls);
-  // Debug.add('pool available: ' + Pool.count());
-
-  Renderer.postRender();
-
-  // EventSystem.printDebug();
-
-  let bytes = window.performance.memory.totalJSHeapSize.toLocaleString();
-  Debug.add(`heap: ${bytes} bytes`);
   Debug.draw();
-  Debug.postRender();
-}
+};
 
-function setup() {
-  let cvs = Utils.getEl('cvs');
-  let ctx = cvs.getContext('2d', { alpha: false });
-  p3 = new P3(cvs, ctx);
-  window.p3 = p3;
 
-  Pool.init();
+// import GameTimer from './core/GameTimer.js';
 
-  scene = new Scene();
+// import P3 from './P3.js';
+// import Scene from './Scene.js';
 
-  Debug.init({toggleKey: 'Escape'});
-  Debug.setOn(window.debug);
 
-  scene.restartGame();
+// import Event from './event/Event.js';
+// import EventSystem from './event/EventSystem.js';
 
-  timer = new GameTimer();
-  timer.update = function(dt) {
-    update(dt);
-    preRender();
-    render();
-    postRender();
-  };
-  timer.start();
-}
+// // Systems
+// import Renderer from './Renderer.js';
 
-setup();
+// import cfg from './cfg.js';
+// import Pool from './core/Pool.js';
+
+
+// // var sound = new Howl({
+// //   src: ['../data/explosion.wav'],
+// //   volume: 0.8
+// // });
+
+// // sound.play();
+
+// window.gameTime = 0;
+// window.gameFrameCount = 0;
+// window.Renderer = Renderer;
+
+// window.debug = false;
+// window.Debug = Debug;
+// window.scene = null;
+
+// window.vec2Ctor = 0;
+// window.clearArrayCalls = 0;
+
+// window.Events = new EventSystem();
+// window.ignoreDirty = false;
+// window.p3 = null;
+
+// let p3;
+// let timer;
+// let perfTimer = new Date();
+
+// let avgDelta = 0;
+// let avgFrames = 0;
+// let avgCalc = 0;
+
+// document.addEventListener('mousedown', e => new Event({ evtName: 'GAME_MOUSE_DOWN', data: e }).fire());
+// document.addEventListener('mouseup', e => new Event({ evtName: 'GAME_MOUSE_UP', data: e }).fire());
+// document.addEventListener('contextmenu', e => e.preventDefault());
+
+// function update(dt) {
+//   Debug.add(`Game time: ${Math.floor(window.gameTime)}`);
+//   Debug.add(`Root Entity count: ${scene.entities.size}`);
+
+//   let totalVec2Calls = window.vec2Ctor.toLocaleString();
+//   Debug.add(`Total Vec2 ctor calls: ${totalVec2Calls}`);
+//   // Debug.add('Bullets: ' + window.count);
+
+//   scene.update(dt);
+
+//   Events.printDebug();
+//   window.gameTime += dt;
+// }
+
+// function preRender() {
+//   perfTimer = new Date().getTime();
+//   Renderer.preRender();
+// }
+
+// function render() {
+//   Renderer.render();
+//   gameFrameCount++;
+// }
+
+// function postRender() {
+//   let timeDiff = new Date().getTime() - perfTimer;
+//   // avgDelta += timeDiff;
+//   // avgFrames++;
+//   // if (avgFrames > 100) {
+//   //   avgCalc = avgDelta / avgFrames;
+//   //   avgFrames = 0;
+//   //   avgDelta = 0;
+//   // }
+
+//   Debug.add('render ms: ' + timeDiff);
+//   // Debug.add('avg render ms: ' + avgCalc);
+//   // Debug.add('clear array calls: ' + window.clearArrayCalls);
+//   // Debug.add('pool available: ' + Pool.count());
+
+//   Renderer.postRender();
+
+//   // EventSystem.printDebug();
+
+//   let bytes = window.performance.memory.totalJSHeapSize.toLocaleString();
+//   Debug.add(`heap: ${bytes} bytes`);
+//   Debug.draw();
+//   Debug.postRender();
+// }
+
+// function setup() {
+//   let cvs = Utils.getEl('cvs');
+//   let ctx = cvs.getContext('2d', { alpha: false });
+//   p3 = new P3(cvs, ctx);
+//   window.p3 = p3;
+
+//   Pool.init();
+
+//   scene = new Scene();
+
+//   Debug.init({toggleKey: 'Escape'});
+//   Debug.setOn(window.debug);
+
+//   scene.restartGame();
+
+//   timer = new GameTimer();
+//   timer.update = function(dt) {
+//     update(dt);
+//     preRender();
+//     render();
+//     postRender();
+//   };
+//   timer.start();
+// }
+
+// setup();
