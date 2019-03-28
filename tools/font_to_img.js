f622d7f5a3b8975bce9f52055f6929d0b7910b69
Andor Saga <andor.saga@gmail.com>
2019-03-28 00:14:55 -0400

    .

--- tools/font_to_img.js
+++ tools/font_to_img.js
@@ -7,38 +7,38 @@ let timer = 0;
 let currChar = 0;
 let sz = 40;
 
+window.preload = function() {
+  // font = loadFont('data/fonts/SourceSansPro-Regular.otf');
+}
+
 window.setup = function(){
 	createCanvas(sz*2, sz*2);
 	
 	textSize(50);
-	
-	textFont('monospace');
-
+	// textFont('monospace');
+	// textFont("Comic Sans MS");
 	textAlign(CENTER, CENTER);
-	
-	fill(0,250,0);
 }
 
 window.draw = function (){
 	background(0);
+	clear();
 
-	if(isDone) return;
+	if(isDone) {
+		textSize(20);
+		text('done!', 40, 40);
+		return;	
+	}
 	
 	timer += 1;
 
 	let c = data[currChar];
 	let jpChar = c.jpChar;
 	let romanji = c.romanji;
-	debugger;
 
 	noStroke();
-	fill(0,255,0)
+	fill(0,0,0)
 	text( jpChar, sz, sz);
-
-	// noFill();
-	// stroke(255);
-	// rect(0, 0, sz*2, sz*2);
-
 	
 	if(timer > 10){
 		save(`${romanji}.png`);
