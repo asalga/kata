2f8512c9a3748608c1837bf479e944fcdea5fbf1
Andor Saga <andor.saga@gmail.com>
2019-04-03 23:31:36 -0400

    .

--- tools/font_to_img.js
+++ tools/font_to_img.js
@@ -12,40 +12,40 @@ window.preload = function() {
   font = loadFont('data/font/light.ttf');
 }
 
-window.setup = function(){
-	createCanvas(sz*2, sz*2);
-	textSize(50);
-	// textFont(font);
-	textAlign(CENTER, CENTER);
+window.setup = function() {
+  createCanvas(sz * 2, sz * 2);
+  textSize(50);
+  // textFont(font);
+  textAlign(CENTER, CENTER);
 }
 
-window.draw = function (){
-	background(0);
-	clear();
+window.draw = function() {
+  background(0);
+  clear();
 
-	if(isDone) {
-		textSize(20);
-		text('done!', 40, 40);
-		return;	
-	}
-	
-	timer += 1;
+  if (isDone) {
+    textSize(20);
+    text('done!', 40, 40);
+    return;
+  }
 
-	let c = data[currChar];
-	let jpChar = c.jpChar;
-	let romanji = c.romanji;
+  timer += 1;
 
-	noStroke();
-	fill(0, 255, 0)
-	text(jpChar, sz, sz);
-	
-	if(timer > 10){
-		save(`${romanji}.png`);
-		timer = 0;
-		currChar++;
+  let c = data[currChar];
+  let jpChar = c.jpChar;
+  let romanji = c.romanji;
 
-		if(currChar === data.length){
-			isDone = true;
-		}
-	}
+  noStroke();
+  fill(0, 255, 0)
+  text(jpChar, sz, sz);
+
+  if (timer > 10) {
+    save(`${romanji}.png`);
+    timer = 0;
+    currChar++;
+
+    if (currChar === data.length) {
+      isDone = true;
+    }
+  }
 }
