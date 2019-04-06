ff1c8627bc81372c4a0567c3c6d9cdac9f0f38a2
Andor Saga <andor.saga@gmail.com>
2019-04-05 23:24:58 -0400

    .

--- src/js/entity/actors/CharSlot.js
+++ src/js/entity/actors/CharSlot.js
@@ -20,24 +20,24 @@ export default function createSlot() {
 
   e.timer = 0;
 
-  let launchChar = function(){
+  let launchChar = function() {
     let glyph = EntityFactory.create('glyph');
     glyph.pos.set(e.pos);
     scene.add(glyph);
   };
-  e.addComponent(new BHVLeaf(e, {exe: launchChar}))
+  e.addComponent(new BHVLeaf(e, { exe: launchChar }))
 
   let spriteRender = new SpriteRender(e, { layerName: 'sprite' });
   spriteRender.draw = function(gfx) {
     gfx.push();
     gfx.stroke(0, 255, 0);
     gfx.noFill();
-    gfx.rect(e.pos.x, e.pos.y, size ,size);
+    gfx.rect(e.pos.x, e.pos.y, size, size);
     gfx.pop();
   };
   e.addComponent(spriteRender);
 
-  
+
 
   e.updateProxy = function(dt) {
 
@@ -45,7 +45,7 @@ export default function createSlot() {
 
     // if(this.timer > 2){
     //   this.timer = 0;
-      
+
     //   let r = Math.floor(p3.random(0,3));
 
     //   if(r < 1){
